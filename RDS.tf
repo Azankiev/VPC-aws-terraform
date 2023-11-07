049fb6b38b8d4c688f4a213834a199c9537712d2
Azankiev <viniciusguilty@outlook.com>
2023-11-06 23:46:57 -0300

    Update RDS.tf

--- RDS.tf
+++ RDS.tf
@@ -4,8 +4,8 @@ resource "aws_db_instance" "banco" {
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   db_name              = "MeuBanco"
-  username             = "admin"
-  password             = "12qwawsx"
+  username             = ""
+  password             = ""
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.db_subnet.id
